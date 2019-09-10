eb7843fe8ba3e91b5f05af8b571c444333d94e03
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-09-10 13:21:49 +0100

    Add numbers

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //import any components needed
-import "./NumberButton";
+import NumberButton from "./NumberButton";
 //Import your array data to from the provided data file
 import {numbers} from "../../../data";
 
@@ -9,9 +9,9 @@ const Numbers = () => {
   // STEP 2 - add the imported data to state
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      {
+          numbers.map(number => <NumberButton number={number} key={number} />)
+      }
     </div>
   );
 };
